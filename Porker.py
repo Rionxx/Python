82ec6c39cdaad7cddf4d4786669b13c9bedeea4e
Rion <rionfx.42@gmail.com>
2021-11-03 01:39:44 +0900

    Debug Result Porker

--- Porker.py
+++ Porker.py
@@ -27,31 +27,45 @@ def check_roll(cards):
   hand_marks = [c.mark for c in hand]
 
   if mark_check(hand_marks) == 4:
-    return "flash!!"
+    return 0 #flash
   if len(set(hand_nums)) == 3:
-    return "1 Pair"
+    return 1 #1 Pair
   elif len(set(hand_nums)) == 2 and pair_check(hand_nums) == 3:
-    return "3 Card"
+    return 2 #3 Card
   elif len(set(hand_nums)) == 2 and pair_check(hand_nums) == 2:
-    return "2 Pair"
+    return 3 #2 Pair
   elif len(set(hand_nums)) == 1:
-    return "4 Card"
+    return 4 #4 Card
   else:
-    high = max(hand_nums)
-    roll = "high card:{}".format(high)
-    return roll
+    return 5 #Buta
+
 
 if __name__=='__main__':
   bill = []
   marks = ('♣︎', '🔸', '❤️', '♠️')
+  roles = ("flash", "1 Pair", "3 Card", "2 Pair", "4 Card", "Buta")
+  role_count = [0 for x in range(len(roles))]
 
-  bill = [card(k, j) for k in marks for j in range(1, 14)]
-  random.shuffle(bill)
+  count = 0
+  while count < 10000:
+    count += 1
 
-  hand = bill[:4]
-  hand_nums = [c.num for c in hand]
-  hand_marks = [c.mark for c in hand]
+    bill = [card(k, j) for k in marks for j in range(1, 14)]
+    random.shuffle(bill)
 
-  for i in range(len(hand_nums)):
-    print("Card", i + 1, ":", hand_nums[i], hand_marks[i])
-  print("handle", check_roll(hand))
+    hand = bill[:4]
+    hand_nums = [c.num for c in hand]
+    hand_marks = [c.mark for c in hand]
+
+    for i in range(len(hand_nums)):
+      print("Card", i + 1, ":", hand_nums[i], hand_marks[i])
+    
+    roll = check_roll(hand)
+    role_count[roll] += 1
+  
+  print("-"*20)
+  print('{}result'.format(count))
+  print("-"*20)
+
+  for i in range(len(roles)):
+    print('{}:{}回 確率:{}%'.format(roles[i], role_count[i], role_count[i] / count * 100))
